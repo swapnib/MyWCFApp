0c62b10c839cb7297347cc4b945d9af582da3539
swapnil.borpe <swapnil.borpe@gmail.com>
2019-05-05 17:37:24 +0530

    Adding the changes for the wcf service

---
+++ WCF Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.ServiceClient svr = new ServiceReference.ServiceClient();
+           Console.WriteLine(svr.AddInt(1, 2));            
+            Console.WriteLine(svr.AddFloat(1.5F,2.6F));
+
+        }
+    }
+}
